c5a16b1ffdbf3fd77bd531f944175a670538ce59
HaNJI <henhooky@gmail.com>
2021-10-18 16:57:07 +0500

    lab_2 complete

--- MPIproject/MPIproject.cpp
+++ MPIproject/MPIproject.cpp
@@ -1,7 +1,8 @@
-// MPIproject.cpp : This file contains the 'main' function. Program execution begins and ends there.
+﻿// MPIproject.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include <iostream>
+#include <string>
 #include "mpi.h"
 
 using namespace std;
@@ -10,40 +11,75 @@ using namespace std;
 #define RECV_PROCESS 1
 #define TAG 0
 
-int SendProcess()
+
+int SendProcess(int data, int world_size, int process)
 {
-    cout << "Start sending!" << endl;
-
-    char* data = (char*) "Hello, I'm sending process!";
-    int size = strlen(data) + 1;
-
-    MPI_Send(data, size, MPI_CHAR, RECV_PROCESS, TAG, MPI_COMM_WORLD);
+    MPI_Send(&data, 1, MPI_INT, (process + 1) % world_size, TAG, MPI_COMM_WORLD);
 
     return 0;
 }
 
-int RecvProcess()
+int RecvProcess(int world_size, int process)
 {
-    cout << "Start receiving!" << endl;
+    //get length
+    int buf;
+    int source = (process - 1);
+    source = source >= 0 ? source : world_size - 1;
     MPI_Status status;
 
-    //get length
-    MPI_Probe(SEND_PROCESS, TAG, MPI_COMM_WORLD, &status);
-    int size = -1;
-    MPI_Get_count(&status, MPI_CHAR, &size);
-    char* data = (char*) malloc(size);
+    MPI_Recv(&buf, 1, MPI_INT, source, TAG, MPI_COMM_WORLD, &status);
 
-    //get message
-    MPI_Recv(data, size, MPI_CHAR, SEND_PROCESS, TAG, MPI_COMM_WORLD, MPI_STATUSES_IGNORE);
-    cout << data << endl;
-
-    free(data);
-
-    return 0;
+    return buf;
 }
+
 
 int main(int argc, char** argv)
 {
+    MPI_Init(&argc, &argv);
+
+    int world_size = -1;
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+    int process = -1;
+    MPI_Comm_rank(MPI_COMM_WORLD, &process);
+
+    cout << "World size: " << world_size << ", process number: " << process << endl;
+
+    if (world_size < 1) {
+        return MPI_Finalize();
+    }
+
+    int count = 0;
+
+    if (process == SEND_PROCESS) 
+    {
+        int msg = 0;
+        SendProcess(msg, world_size, process);
+    }
+
+
+    while (count < 2) 
+    {
+        int msg = RecvProcess(world_size, process);
+        cout << "process number - " << process << " says: " << msg << endl;
+        msg++;
+        if (count == 1 && process == SEND_PROCESS) 
+        {
+            break;
+        }
+        SendProcess(msg, world_size, process);
+
+        
+        //последнюю отправку никто не принимает
+        count++;
+
+        
+    }
+
+
+    return MPI_Finalize();
+}
+    /* Первая задача
     MPI_Init(&argc, &argv);
 
     int world_size = -1;
@@ -75,3 +111,4 @@ int main(int argc, char** argv)
     return MPI_Finalize();;
 }
 
+*/
