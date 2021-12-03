ff7787911287c009467307b9f8bce6e1eba3792e
HaNJI <henhooky@gmail.com>
2021-12-03 16:39:36 +0500

    My_Gather operation completed

--- MPIproject/MPIproject.cpp
+++ MPIproject/MPIproject.cpp
@@ -9,71 +9,57 @@
 
 using namespace std;
 
-
-#define SEND_PROCESS 0
-#define RECV_PROCESS 1
 #define TAG 0
-#define OP_LONG 100000
+#define ROOT_PROCESS 0
+#define OP_LONG 10000
+
+void My_Gather(int* sendarray, int sendcount, MPI_Datatype sendtype, int* recvbuf, int recvcount, MPI_Datatype recvtype, int root, MPI_Comm comm) {
+    int process = -1;
+    MPI_Comm_rank(comm, &process);
+
+    if (process == -1) {
+        throw "error: process doesn't exist";
+    }
+
+    if (process != root) {
+        MPI_Send(sendarray, sendcount, sendtype, root, TAG, comm);
+    }
 
 
-int SendProcess()
-{
-    cout << "Start sending!" << endl;
+    if (process == root) {
 
-    char* data = (char*) "Hello, I'm sending process!";
-    int size = strlen(data) + 1;
+        int comm_size = -1;
+        MPI_Comm_size(comm, &comm_size);
 
-    double start = MPI_Wtime();
+        if (process == -1) {
+            throw "error: can't find world size";
+        }
 
-    MPI_Request* request = (MPI_Request*) malloc(sizeof(MPI_Request) * OP_LONG);
-    for (int i = 0; i < OP_LONG; i++) {
         
-        MPI_Isend(data, size, MPI_CHAR, RECV_PROCESS, TAG, MPI_COMM_WORLD, &request[i]);
+
+        for (int i = 0; i < comm_size; i++) {
+            if (i != root) {
+                MPI_Status status;
+                int* loc_rbuf = (int*)malloc(recvcount * sizeof(int));
+                MPI_Recv(loc_rbuf, recvcount, recvtype, i, TAG, comm, &status);
+
+                for (int j = 0; j < recvcount; j++) {
+                    int k = i * recvcount + j;
+                    recvbuf[k] = loc_rbuf[j];
+;               }
+
+                //MPI_Probe(i, TAG, comm, &status);
+            }
+            else {
+                for (int j = 0; j < recvcount; j++) {
+                    int k = i * recvcount + j;
+                    recvbuf[k] = sendarray[j];
+                }
+            }
+        }
     }
-
-    MPI_Status* statuses = (MPI_Status*)malloc(sizeof(MPI_Status) * OP_LONG);
-
-    MPI_Waitall(OP_LONG, request, statuses);
-    
-    double end = MPI_Wtime();
-    double dif = end - start;
-
-    double realDif = (double)dif * 1000 / (double)OP_LONG;
-
-    cout << "Time spent on the send operation (ms): " << realDif << endl;
-
-    return 0;
 }
 
-int RecvProcess() 
-{
-    cout << "Start receiving!" << endl;
-    MPI_Status status;
-    
-    //get length
-    MPI_Probe(SEND_PROCESS, TAG, MPI_COMM_WORLD, &status);
-    int size = -1;
-    MPI_Get_count(&status, MPI_CHAR, &size);
-    char* data = (char*) malloc(size);
-    //here we've already known that Send is started
-
-    
-    //get message
-    for (int i = 0; i < OP_LONG; i++) {
-        MPI_Request request;
-        MPI_Irecv(data, size, MPI_CHAR, SEND_PROCESS, TAG, MPI_COMM_WORLD, &request);
-
-        MPI_Status status;
-        MPI_Wait(&request, &status);
-    }
-
-    
-    cout << data << endl;
-
-    free(data);
-
-    return 0;
-}
 
 int main(int argc, char** argv)
 {
@@ -86,24 +72,44 @@ int main(int argc, char** argv)
     MPI_Comm_rank(MPI_COMM_WORLD, &process);
 
     cout << "World size: " << world_size << ", process number: " <<  process << endl;
+    int sendarray[2]{ process, -1 };
+    int root = ROOT_PROCESS, * rbuf;
+    rbuf = (int*)malloc(world_size * 2 * sizeof(int));
 
     
+    double start = MPI_Wtime();
+    for (int i = 0; i < OP_LONG; i++) {
+        MPI_Gather(sendarray, 2, MPI_INT, rbuf, 2, MPI_INT, root, MPI_COMM_WORLD);
+    }
+    double end = MPI_Wtime();
+    double dif = end - start;
 
-    if (world_size > 1)
-    {
-        switch (process)
-        {
-        case SEND_PROCESS:
-            SendProcess();
-            break;
-        case RECV_PROCESS:
-            RecvProcess();
-            break;
-        default:
-            cout << "Process number: #" << process << " doesn't have a task!" << endl;
-            break;
+    if (process == root) {
+        double realDif = (double)dif * 1000 / (double)OP_LONG;
+        cout << "Time spent to the MPI_Gather operation (ms): " << realDif << endl;
+    }
+    
+    
+    start = MPI_Wtime();
+    for (int i = 0; i < OP_LONG; i++) {
+        My_Gather(sendarray, 2, MPI_INT, rbuf, 2, MPI_INT, root, MPI_COMM_WORLD);
+    }
+    end = MPI_Wtime();
+    dif = end - start;
+
+    if (process == root) {
+        double realDif = (double)dif * 1000 / (double)OP_LONG;
+        cout << "Time spent to the My_Gather operation (ms): " << realDif << endl;
+    }
+
+    /*
+    if (process == ROOT_PROCESS) {
+        for (int i = 0; i < 2 * world_size; i++) {
+            cout << rbuf[i] << endl;
         }
     }
+    */
+    
 
     return MPI_Finalize();;
 }
